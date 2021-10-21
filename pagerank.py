543f96cdb66b0e71c6145b24582128d11a8b6622
Leirof <vince.lrf@gmail.com>
2021-10-21 17:31:36 +0200

    First commit

---
+++ pagerank.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+def getS(A):
+    k = getKout(A)
+
+    S = np.zeros([len(A),len(A)])
+    for row in range(len(A)):
+        for column in range(len(A[row])):
+            if k[column] != 0:
+                S[row,column] = A[row, column] / k[column]
+            else:
+                S[row,column] = 1/len(A)
+    return S
+
+def getKout(A):
+    k = np.zeros(len(A))
+    for j in range(len(A)):
+        for i in range(len(A[j])):
+            k[i] = k[i] + A[j,i]
+    return k
+
+def getKin(A):
+    k = np.zeros(len(A))
+    for j in range(len(A)):
+        for i in range(len(A[j])):
+            k[j] = k[j] + A[j,i]
+    return k
+
+def getP(S, j0, order = 0):
+    p = np.zeros(len(S))
+    p[j0] = 1
+
+    for n in range(order):
+        p = S.dot(p)
+
+    return p
+
+def getG(S, alpha, v):
+    G = np.zeros([len(S),len(S)])
+    for i in range(len(S)):
+        for j in range(len(S[i])):
+            G[i,j] = alpha * S[i,j] + (1-alpha) * v[i]
+
+    return G
+
+def surf(G, P, epsilon = 1e-10):
+    order = 0
+    while(diff(G.dot(P),P) >= epsilon):
+        P = G.dot(P)
+        order += 1
+    return P, order
+
+def diff(a,b):
+    res = 0
+    for i in range(len(a)):
+        res += abs(a[i] - b[i])
+
+    return res
+
+A = np.array([
+    [0, 0, 1, 0, 0, 0],
+    [1, 0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1],
+    [0, 0, 1, 1, 0, 0],
+    [0, 0, 0, 1, 1, 0]
+])
+
+S = getS(A)
+
+v = np.ones(len(S)) / len(S)
+#j0 = 3                          #np.random.randint(1, len(A))
+#val, vec = np.linalg.eig(G)
+P = 1/6 * np.array([1,1,1,1,1,1]) # getP(S, j0, 0)
+epsilon = 1e-10
+
+# 2.1 - 1
+alpha = 0.85
+G = getG(S, alpha, v)
+print(f"\n2.1)\n   > For a random surfer that start his journey on a random node and with a dumping facotr at {round(alpha,2)}, he have these chances to finish at each node:\n")
+P, step = surf(G,P,epsilon)
+for i in range(len(P)):
+    print(f"      Node {i} : {P[i]}")
+print(f"\n      This stability is reached after {step} steps")
+
+# 2.1 - 2
+alpha = np.random.rand() / 2 + 0.5
+G = getG(S, alpha, v)
+print(f"\n   > Now, if we change the dumping factor to {round(alpha,2)}, the surfer have these chances to finish at each node:\n")
+P, step = surf(G,P,epsilon)
+for i in range(len(P)):
+    print(f"      Node {i+1} : {P[i]}")
+print(f"\n      This stability is reached after {step} steps")
